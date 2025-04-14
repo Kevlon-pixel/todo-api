e12d0282648c9a46f95961cb500866d69384bd07
vladyslav <vl0d1sla8@mail.ru>
2025-04-14 22:02:05 +0300

    feat(db): almost done

--- src/database/queries.js
+++ src/database/queries.js
@@ -10,7 +10,7 @@ router.get('/todos', async (req, res) => {
     }
     catch (error) {
       console.error(error.message);
-      res.status(500).send('Server error!');
+      res.status(500).send('Server error');
     }
   });
   
@@ -25,8 +25,54 @@ router.post('/todos', async (req, res) => {
     }
     catch (error) {
       console.error(error.message);
-      res.status(500).send('Server error!');
+      res.status(500).send('Server error');
     }
   });
+
+router.post('/change_completed', async (req, res) => {
+    try {
+        const {todo_id, completed} = req.body;
+        
+        if (typeof todo_id != 'number' || typeof completed !== 'boolean') {
+            return res.status(400).send('Invalid input');
+        }
+
+        await pool.query(
+            `UPDATE "todos" SET "completed" = $1 WHERE "todo_id" = $2`,
+            [completed, todo_id]
+        );
+
+        res.send('Status updated')
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+});
+
+router.delete('/todo_delete', async (req, res) => {
+    try {
+        const {todo_id} = req.body;
+
+        if (typeof todo_id !== 'number') {
+            return res.status(400).send('Invalid input');
+        }
+
+        const result = await pool.query(
+            'DELETE FROM "todos" WHERE "todo_id" = $1',
+            [todo_id]
+        );
+
+        if (result.rowCount === 0) {
+            return res.status(404).send('Todo not found');
+        }
+
+        res.send('Todo deleted');
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error')
+    }
+});
 
 module.exports = router;
